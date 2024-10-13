957470c1b3ae66aa5a14e3c4466c3faa42e73cf0
Tohrusky <65994850+Tohrusky@users.noreply.github.com>
2024-10-13 22:39:04 +0100

    feat: support custom cache folder && use gh-proxy to download

--- ccrestoration/cache_models/__init__.py
+++ ccrestoration/cache_models/__init__.py
@@ -23,7 +23,11 @@ def get_file_sha256(file_path: str, blocksize: int = 1 << 20) -> str:
 
 
 def load_file_from_url(
-    config: BaseConfig, force_download: bool = False, progress: bool = True, model_dir: Optional[str] = None
+    config: BaseConfig,
+    force_download: bool = False,
+    progress: bool = True,
+    model_dir: Optional[str] = None,
+    gh_proxy: Optional[str] = None,
 ) -> str:
     """
     Load file form http url, will download models if necessary.
@@ -34,6 +38,7 @@ def load_file_from_url(
     :param force_download: Whether to force download the file.
     :param progress: Whether to show the download progress.
     :param model_dir: The path to save the downloaded model. Should be a full path. If None, use default cache path.
+    :param gh_proxy: The proxy for downloading from github release. Example: https://github.abskoop.workers.dev/
     :return:
     """
 
@@ -42,13 +47,21 @@ def load_file_from_url(
 
     cached_file_path = os.path.abspath(os.path.join(model_dir, config.name))
 
+    _url: str = str(config.url)
+    _gh_proxy = gh_proxy
+    if _gh_proxy is not None and _url.startswith("https://github.com"):
+        if not _gh_proxy.endswith("/"):
+            _gh_proxy += "/"
+        _url = _gh_proxy + _url
+        print(f"Using github proxy: {_gh_proxy}")
+
     if not os.path.exists(cached_file_path) or force_download:
-        print(f"Downloading: {config.url} to {cached_file_path}\n")
+        print(f"Downloading: {_url} to {cached_file_path}\n")
 
         @retry(wait=wait_random(min=3, max=5), stop=stop_after_delay(10) | stop_after_attempt(30))
         def _download() -> None:
             try:
-                download_url_to_file(str(config.url), cached_file_path, hash_prefix=None, progress=progress)
+                download_url_to_file(url=_url, dst=cached_file_path, hash_prefix=None, progress=progress)
             except Exception as e:
                 print(f"Download failed: {e}, retrying...")
                 raise e
--- tests/test_cache_models.py
+++ tests/test_cache_models.py
@@ -4,3 +4,16 @@ from ccrestoration.cache_models import load_file_from_url
 
 def test_cache_models() -> None:
     load_file_from_url(CONFIG_REGISTRY.get(ConfigType.RealESRGAN_AnimeJaNai_HD_V3_Compact_2x))
+
+
+def test_cache_models_with_gh_proxy() -> None:
+    load_file_from_url(
+        config=CONFIG_REGISTRY.get(ConfigType.RealESRGAN_AnimeJaNai_HD_V3_Compact_2x),
+        force_download=True,
+        gh_proxy="https://github.abskoop.workers.dev/",
+    )
+    load_file_from_url(
+        config=CONFIG_REGISTRY.get(ConfigType.RealESRGAN_AnimeJaNai_HD_V3_Compact_2x),
+        force_download=True,
+        gh_proxy="https://github.abskoop.workers.dev",
+    )
