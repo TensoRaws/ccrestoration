af59ebb2b2486e1454b47167dc1b0b6b7037ff7a
Tohru <65994850+Tohrusky@users.noreply.github.com>
2024-12-10 17:33:45 +0000

    feat: optimize cuda for vapoursynth (#27)
    * feat: optimize cuda for vapoursynth
    
    * doc: update README.md

--- ccrestoration/vs/sr.py
+++ ccrestoration/vs/sr.py
@@ -1,3 +1,10 @@
+"""
+Reference: https://github.com/HolyWu/vs-realesrgan
+
+Thanks to HolyWu for his great work on super-resolution.
+"""
+
+from threading import Lock
 from typing import Any, Callable, Union
 
 import torch
@@ -11,8 +18,6 @@ def inference_sr(
     clip: vs.VideoNode,
     scale: Union[float, int, Any],
     device: torch.device,
-    _frame_to_tensor: Callable[[vs.VideoFrame, torch.device], torch.Tensor] = frame_to_tensor,
-    _tensor_to_frame: Callable[[torch.Tensor, vs.VideoFrame], vs.VideoFrame] = tensor_to_frame,
 ) -> vs.VideoNode:
     """
     Inference the video with the model, the clip should be a vapoursynth clip
@@ -21,20 +26,94 @@ def inference_sr(
     :param clip: vs.VideoNode
     :param scale: The scale factor
     :param device: The device
-    :param _frame_to_tensor: The function to convert the frame to tensor
-    :param _tensor_to_frame: The function to convert the tensor to frame
     :return:
     """
 
     if clip.format.id not in [vs.RGBH, vs.RGBS]:
         raise vs.Error("Only vs.RGBH and vs.RGBS formats are supported")
 
+    if device.type == torch.device("cuda").type:
+        return inference_sr_cuda(inference, clip, scale, device)
+    else:
+        return inference_sr_general(inference, clip, scale, device)
+
+
+def inference_sr_general(
+    inference: Callable[[torch.Tensor], torch.Tensor],
+    clip: vs.VideoNode,
+    scale: Union[float, int, Any],
+    device: torch.device,
+) -> vs.VideoNode:
+    """
+    Inference for General devices
+
+    :param inference: The inference function
+    :param clip: vs.VideoNode
+    :param scale: The scale factor
+    :param device: The device
+    :return:
+    """
+
+    f2t_stream_lock = Lock()
+    inf_stream_lock = Lock()
+    t2f_stream_lock = Lock()
+
     def _inference(n: int, f: list[vs.VideoFrame]) -> vs.VideoFrame:
-        img = _frame_to_tensor(f[0], device).unsqueeze(0)
+        with f2t_stream_lock:
+            img = frame_to_tensor(f[0], device).unsqueeze(0)
 
-        output = inference(img)
+        with inf_stream_lock:
+            output = inference(img)
 
-        return _tensor_to_frame(output, f[1].copy())
+        with t2f_stream_lock:
+            res = tensor_to_frame(output, f[1].copy())
+
+        return res
+
+    new_clip = clip.std.BlankClip(width=clip.width * scale, height=clip.height * scale, keep=True)
+    return new_clip.std.FrameEval(
+        lambda n: new_clip.std.ModifyFrame([clip, new_clip], _inference), clip_src=[clip, new_clip]
+    )
+
+
+def inference_sr_cuda(
+    inference: Callable[[torch.Tensor], torch.Tensor],
+    clip: vs.VideoNode,
+    scale: Union[float, int, Any],
+    device: torch.device,
+) -> vs.VideoNode:
+    """
+    Inference for CUDA devices
+
+    :param inference: The inference function
+    :param clip: vs.VideoNode
+    :param scale: The scale factor
+    :param device: The device
+    :return:
+    """
+
+    f2t_stream_lock = Lock()
+    inf_stream_lock = Lock()
+    t2f_stream_lock = Lock()
+
+    f2t_stream = torch.cuda.Stream(device)
+    inf_stream = torch.cuda.Stream(device)
+    t2f_stream = torch.cuda.Stream(device)
+
+    def _inference(n: int, f: list[vs.VideoFrame]) -> vs.VideoFrame:
+        with f2t_stream_lock, torch.cuda.stream(f2t_stream):
+            img = frame_to_tensor(f[0], device).unsqueeze(0)
+            f2t_stream.synchronize()
+
+        with inf_stream_lock, torch.cuda.stream(inf_stream):
+            output = inference(img)
+            inf_stream.synchronize()
+
+        with t2f_stream_lock, torch.cuda.stream(t2f_stream):
+            res = tensor_to_frame(output, f[1].copy())
+            t2f_stream.synchronize()
+
+        return res
 
     new_clip = clip.std.BlankClip(width=clip.width * scale, height=clip.height * scale, keep=True)
     return new_clip.std.FrameEval(
--- ccrestoration/vs/vsr.py
+++ ccrestoration/vs/vsr.py
@@ -1,3 +1,4 @@
+from threading import Lock
 from typing import Any, Callable, Dict, Union
 
 import torch
@@ -12,11 +13,43 @@ def inference_vsr(
     scale: Union[float, int, Any],
     length: int,
     device: torch.device,
-    _frame_to_tensor: Callable[[vs.VideoFrame, torch.device], torch.Tensor] = frame_to_tensor,
-    _tensor_to_frame: Callable[[torch.Tensor, vs.VideoFrame], vs.VideoFrame] = tensor_to_frame,
+    one_frame_out: bool = False,
 ) -> vs.VideoNode:
     """
     Inference the video with the model, the clip should be a vapoursynth clip
+
+    :param inference: The inference function
+    :param clip: vs.VideoNode
+    :param scale: The scale factor
+    :param length: The length of the input frames
+    :param device: The device
+    :param one_frame_out: Whether the model is one frame output model
+    :return:
+    """
+    if clip.format.id not in [vs.RGBH, vs.RGBS]:
+        raise vs.Error("Only vs.RGBH and vs.RGBS formats are supported")
+
+    if length > clip.num_frames:
+        raise ValueError("The length of the input frames should be less than the number of frames in the clip")
+
+    if length < 2:
+        raise ValueError("The length of the input frames should be greater than 1")
+
+    if not one_frame_out:
+        return inference_vsr_multi_frame_out(inference, clip, scale, length, device)
+    else:
+        return inference_vsr_one_frame_out(inference, clip, scale, length, device)
+
+
+def inference_vsr_multi_frame_out(
+    inference: Callable[[torch.Tensor], torch.Tensor],
+    clip: vs.VideoNode,
+    scale: Union[float, int, Any],
+    length: int,
+    device: torch.device,
+) -> vs.VideoNode:
+    """
+    VSR for multi frame output models
 
     f1, f2, f3, f4 -> f1', f2', f3', f4'
 
@@ -28,43 +61,37 @@ def inference_vsr(
     :param scale: The scale factor
     :param length: The length of the input frames
     :param device: The device
-    :param _frame_to_tensor: The function to convert the frame to tensor
-    :param _tensor_to_frame: The function to convert the tensor to frame
     :return:
     """
 
-    if clip.format.id not in [vs.RGBH, vs.RGBS]:
-        raise vs.Error("Only vs.RGBH and vs.RGBS formats are supported")
-
-    if length > clip.num_frames:
-        raise ValueError("The length of the input frames should be less than the number of frames in the clip")
-
-    if length < 2:
-        raise ValueError("The length of the input frames should be greater than 1")
-
     cache: Dict[int, torch.Tensor] = {}
 
+    lock = Lock()
+
     def _inference(n: int, f: list[vs.VideoFrame]) -> vs.VideoFrame:
-        if n not in cache:
-            cache.clear()
+        with lock:
+            if n not in cache:
+                cache.clear()
 
-            img = []
-            for i in range(length):
-                index = n + i
-                if index >= clip.num_frames:
-                    img.append(frame_to_tensor(clip.get_frame(clip.num_frames - 1), device=device).unsqueeze(0))
+                img = []
+                for i in range(length):
+                    index = n + i
+                    if index >= clip.num_frames:
+                        img.append(frame_to_tensor(clip.get_frame(clip.num_frames - 1), device=device).unsqueeze(0))
 
-                else:
-                    img.append(frame_to_tensor(clip.get_frame(n + i), device=device).unsqueeze(0))
+                    else:
+                        img.append(frame_to_tensor(clip.get_frame(n + i), device=device).unsqueeze(0))
 
-            img = torch.stack(img, dim=1)
+                img = torch.stack(img, dim=1)
 
-            output = inference(img)
+                output = inference(img)
 
-            for i in range(output.shape[0]):
-                cache[n + i] = output[0, i, :, :, :]
+                for i in range(output.shape[0]):
+                    cache[n + i] = output[0, i, :, :, :]
 
-        return _tensor_to_frame(cache[n], f[1].copy())
+            res = tensor_to_frame(cache[n], f[1].copy())
+
+        return res
 
     new_clip = clip.std.BlankClip(width=clip.width * scale, height=clip.height * scale, keep=True)
     return new_clip.std.ModifyFrame([clip, new_clip], _inference)
@@ -76,11 +103,9 @@ def inference_vsr_one_frame_out(
     scale: Union[float, int, Any],
     length: int,
     device: torch.device,
-    _frame_to_tensor: Callable[[vs.VideoFrame, torch.device], torch.Tensor] = frame_to_tensor,
-    _tensor_to_frame: Callable[[torch.Tensor, vs.VideoFrame], vs.VideoFrame] = tensor_to_frame,
 ) -> vs.VideoNode:
     """
-    Inference the video with the model, the clip should be a vapoursynth clip
+    VSR for one frame output models
 
     f-2, f-1, f0, f1, f2 -> f0'
 
@@ -92,41 +117,35 @@ def inference_vsr_one_frame_out(
     :param scale: The scale factor
     :param length: The length of the input frames, should be odd
     :param device: The device
-    :param _frame_to_tensor: The function to convert the frame to tensor
-    :param _tensor_to_frame: The function to convert the tensor to frame
     :return:
     """
-
-    if clip.format.id not in [vs.RGBH, vs.RGBS]:
-        raise vs.Error("Only vs.RGBH and vs.RGBS formats are supported")
-
-    if length > clip.num_frames:
-        raise ValueError("The length of the input frames should be less than the number of frames in the clip")
 
     if length % 2 == 0:
         raise ValueError("The length of the input frames should be odd")
 
-    if length < 2:
-        raise ValueError("The length of the input frames should be greater than 1")
+    lock = Lock()
 
     def _inference(n: int, f: list[vs.VideoFrame]) -> vs.VideoFrame:
-        img = []
-        for i in range(length):
-            index = i - length // 2 + n
-            if index < 0:
-                img.append(frame_to_tensor(clip.get_frame(0), device=device).unsqueeze(0))
+        with lock:
+            img = []
+            for i in range(length):
+                index = i - length // 2 + n
+                if index < 0:
+                    img.append(frame_to_tensor(clip.get_frame(0), device=device).unsqueeze(0))
 
-            elif index >= clip.num_frames:
-                img.append(frame_to_tensor(clip.get_frame(clip.num_frames - 1), device=device).unsqueeze(0))
+                elif index >= clip.num_frames:
+                    img.append(frame_to_tensor(clip.get_frame(clip.num_frames - 1), device=device).unsqueeze(0))
 
-            else:
-                img.append(frame_to_tensor(clip.get_frame(index), device=device).unsqueeze(0))
+                else:
+                    img.append(frame_to_tensor(clip.get_frame(index), device=device).unsqueeze(0))
 
-        img = torch.stack(img, dim=1)
+            img = torch.stack(img, dim=1)
 
-        output = inference(img)
+            output = inference(img)
 
-        return tensor_to_frame(output[0, 0, :, :, :], f[1].copy())
+            res = tensor_to_frame(output[0, 0, :, :, :], f[1].copy())
+
+        return res
 
     new_clip = clip.std.BlankClip(width=clip.width * scale, height=clip.height * scale, keep=True)
     return new_clip.std.ModifyFrame([clip, new_clip], _inference)
